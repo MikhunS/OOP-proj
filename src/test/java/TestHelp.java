8f15b18d68acdf0aa6c6e94f43cff36466ab6e24
MikhunS <150695058+MikhunS@users.noreply.github.com>
2023-12-18 17:42:24 +0500

    Update TestHelp.java

--- src/test/java/TestHelp.java
+++ src/test/java/TestHelp.java
@@ -1,12 +1,19 @@
+import org.example.Dialogue;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class HelpTest{
-    public void testHelp(String actual)
+    public void testHelp()
     {
-        String expected = "--help";
-        Assert.assertEquals(expected,actual);
+        String expected = "Для вычисления неопределенного интеграла введите -u";
+        ArrayList<String> actual = new ArrayList<>();
+        actual.add("--help");
+        Dialogue d = new Dialogue();
+        Assert.assertEquals(expected,d.help(actual));
+
     };
 }
