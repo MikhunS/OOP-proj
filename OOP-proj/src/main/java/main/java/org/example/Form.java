145bbaef79dfcd2057dc548185df44d22954e973
MikhunS <150695058+MikhunS@users.noreply.github.com>
2023-12-22 16:03:13 +0500

    Update Form.java

--- OOP-proj/src/main/java/main/java/org/example/Form.java
+++ OOP-proj/src/main/java/main/java/org/example/Form.java
@@ -8,21 +8,21 @@ public class Form{
     public static int func_num;
     public static double down;
     public static double up;
-    public static int accur;
+    public static int accuracy;
 
 
 
     public void setFunc_num(int num){
         func_num=num;
     }
-    public void setDowm(double down){
+    public void setDown(double down){
         Form.down =down;
     }
     public void setUpp(double up){
         Form.up =up;
     }
-    public void setAccur(int accur){
-        Form.accur =accur;
+    public void setAccuracy(int accuracy){
+        Form.accuracy =accuracy;
     }
     public void setFields(){
         fields = fields +1;
