598cbc44018fec98acd00c82661fd03e1307a42a
MikhunS <150695058+MikhunS@users.noreply.github.com>
2023-12-22 20:20:17 +0500

    Update IntegrationTest.java

--- src/test/java/IntegrationTest.java
+++ src/test/java/IntegrationTest.java
@@ -9,34 +9,9 @@ public class IntegrationTest {
     @Test
     public void testIntegrate()
     {
-        String[] input = {"1","10","20","50"};
-        String result;
-        Form form = new Form();
         String expected = "1.4599198195963226";
-        Integration i = new Integration();
-        for (int k = 0;k < 4;k++) {
-            switch (Form.fields) {
-                case 0:
-                    form.setFunc_num(Integer.parseInt(input[k]));
-                    form.setFields();
-                    break;
-                case 1:
-                    form.setDown(Double.parseDouble(input[k]));
-                    form.setFields();
-                    break;
-                case 2:
-                    form.setUpp(Double.parseDouble(input[k]));
-                    form.setFields();
-                    break;
-                case 3:
-                    form.setAccuracy(Integer.parseInt(input[k]));
-                    form.setFields();
-                    break;
-            }
-            if (Form.fields == 4) {
-                result = i.calc_shell(form.func_num, form.down, form.up, form.accuracy);
-                Assert.assertEquals(expected,result);
-            }
-        }
+        IntegrationTestPrep res = new IntegrationTestPrep();
+        String result = res.resultTest();
+        Assert.assertEquals(expected,result);
     }
 }
