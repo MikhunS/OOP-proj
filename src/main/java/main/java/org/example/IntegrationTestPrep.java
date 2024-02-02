deb42fec9051f5057c37723e9dc97380579f1cfb
MikhunS <150695058+MikhunS@users.noreply.github.com>
2024-02-02 19:16:01 +0500

    Update IntegrationTestPrep.java

--- src/main/java/main/java/org/example/IntegrationTestPrep.java
+++ src/main/java/main/java/org/example/IntegrationTestPrep.java
@@ -2,32 +2,29 @@ package main.java.org.example;
 
 
 public class IntegrationTestPrep {
-    public String getResult(String[] input) {
+    public String getResult(String input,Form form) {
         String result="";
-        Form form = new Form();
         Integration i = new Integration();
-        for (int k = 0; k < 4; k++) {
-            switch (Form.fields) {
-                case 0:
-                    form.setFunc_num(Integer.parseInt(input[k]));
-                    form.setFields();
-                    break;
-                case 1:
-                    form.setDown(Double.parseDouble(input[k]));
-                    form.setFields();
-                    break;
-                case 2:
-                    form.setUpp(Double.parseDouble(input[k]));
-                    form.setFields();
-                    break;
-                case 3:
-                    form.setAccuracy(Integer.parseInt(input[k]));
-                    form.setFields();
-                    break;
-            }
-            if (Form.fields == 4) {
-                result = i.calc_shell(form.func_num, form.down, form.up, form.accuracy);
-            }
+        switch (form.fields) {
+            case 0:
+                form.setFunc_num(Integer.parseInt(input));
+                form.setFields();
+                break;
+            case 1:
+                form.setDown(Double.parseDouble(input));
+                form.setFields();
+                break;
+            case 2:
+                form.setUpp(Double.parseDouble(input));
+                form.setFields();
+                break;
+            case 3:
+                form.setAccuracy(Integer.parseInt(input));
+                form.setFields();
+                break;
+        }
+        if (Form.fields == 4) {
+            result = i.calc_shell(form.func_num, form.down, form.up, form.accuracy);
         }
         return result;
     }
