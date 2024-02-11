7e27aeb20215b89e2f98b02694ec93b403be6543
MikhunS <150695058+MikhunS@users.noreply.github.com>
2024-02-11 18:19:56 +0500

    Update TGBot.java

--- src/main/java/main/java/org/example/TGBot.java
+++ src/main/java/main/java/org/example/TGBot.java
@@ -1,18 +1,18 @@
 package main.java.org.example;
 
+import org.jfree.chart.ChartUtils;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.awt.*;
+import java.io.*;
+
 
 
 public class TGBot extends TelegramLongPollingBot {
-
 
 
     @Override
@@ -21,14 +21,14 @@ public class TGBot extends TelegramLongPollingBot {
     }
 
     @Override
-     public String getBotToken() {
+    public String getBotToken() {
         String token = Token.token;
         return token;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
-        Form form =new Form();
+        Form form = new Form();
         String chatId = update.getMessage().getChatId().toString();
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(chatId);
@@ -49,14 +49,26 @@ public class TGBot extends TelegramLongPollingBot {
                 }
                 break;
             case "-show":
-                EventQueue.invokeLater(() -> {
+                var ex = new Graph((int) Form.up, (int) Form.down, Form.func_num);
+                File file = new File("graph.png");
+                try {
+                    ChartUtils.saveChartAsPNG(file, ex.chart, 800, 600);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                SendPhoto sendPhoto = new SendPhoto();
+                sendPhoto.setChatId(String.valueOf(chatId));
+                sendPhoto.setPhoto(new InputFile(file));
 
-                    var ex = new Graph((int) Form.up,(int) Form.down,Form.func_num);
-                    ex.setVisible(true);
-                });
+                try {
+                    execute(sendPhoto);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+                break;
             default:
                 d.enter(chatId, Form.fields);
-                String result = ing.getResult(arg,form);
+                String result = ing.getResult(arg, form);
                 if (Form.fields == 4) {
                     sendMessage.setChatId(chatId);
                     sendMessage.setText("Integral is: " + result);
@@ -71,7 +83,7 @@ public class TGBot extends TelegramLongPollingBot {
                     } catch (TelegramApiException e) {
                         throw new RuntimeException(e);
                     }
-                    Form.fields=0;
+                    Form.fields = 0;
                 }
         }
     }
